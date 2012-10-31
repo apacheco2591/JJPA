55bc010b305eabe79e39135ffda037a1dd5117ce
Paez <jfpaez23@gmail.com>
2012-10-31 16:19:32 +0100

    Prueba Git by Javi

---
+++ Prueba Git/src/pruebaGit.java
@@ -0,0 +1,16 @@
+
+public class pruebaGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int x = 2;
+		int y = 2;
+		
+		System.out.println("2 + 2 = "+(x+y));
+		System.out.println("YEAH :D");
+
+	}
+
+}
